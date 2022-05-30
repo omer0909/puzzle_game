45d9343df8c487127033d4711aab0f018c137188
Omer Faruk Oz <ozomerfaruk98@gmail.com>
2022-05-30 23:17:19 +0300

    add apk file

---
+++ HexFall/Temp/gradleOut/launcher/build/generated/source/buildConfig/release/com/DefaultCompany/HexFall/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.DefaultCompany.HexFall;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.DefaultCompany.HexFall";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # puzzle_game
+
+[Download APK](https://github.com/omer0909/puzzle_game/raw/master/build.apk)
